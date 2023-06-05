2d6f16072dc5c1c5afb2e4c37b7f2d56527c52fb
MaxiBelli <maximiliano_belli@hotmail.com>
2023-06-05 13:40:48 -0300

    feat: Create progress bar and implement page navigation

---
+++ pages/total.js
@@ -0,0 +1,10 @@
+import Layout from "../layout/Layout";
+
+export default function Total() {
+  return (
+    <Layout page="Total and Confirm Order">
+      <h1 className="text-4xl font-black">Total and Confirm Order</h1>
+      <p className="text-2xl my-10">Confirm Your Order Below</p>
+    </Layout>
+  );
+}
